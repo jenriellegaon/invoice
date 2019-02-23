6bdb36de21162fa8077f467fb131ac442af00d4b
Jenrielle Gaon <jenriellegaon@icloud.com>
2019-02-23 23:14:47 +0800

    Change version to 1.0

--- invoice/startFabric.sh
+++ invoice/startFabric.sh
@@ -26,8 +26,8 @@ cd ../basic-network
 # Now launch the CLI container in order to install, instantiate chaincode
 docker-compose -f ./docker-compose.yml up -d cli
 
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n invoice -v 7.0 -p "$CC_SRC_PATH" -l "$LANGUAGE"
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n invoice -l "$LANGUAGE" -v 7.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n invoice -v 1.0 -p "$CC_SRC_PATH" -l "$LANGUAGE"
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n invoice -l "$LANGUAGE" -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
 sleep 10
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n invoice -c '{"function":"initLedger","Args":[""]}'
 
@@ -52,5 +52,5 @@ printf "The 'node query.js' may be run at anytime once the user has been registe
 # Copy packages from $GOPATH under chaincode.
 
 
-#peer chaincode install -n invoice -v 7.2 -l "golang" -p "github.com/invoice/go"
-#peer chaincode upgrade -n invoice -v 7.2 -o orderer.example.com:7050 -C mychannel -l "golang" -p "github.com/invoice/go" -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+#peer chaincode install -n invoice -v 1.1 -l "golang" -p "github.com/invoice/go"
+#peer chaincode upgrade -n invoice -v 1.1 -o orderer.example.com:7050 -C mychannel -l "golang" -p "github.com/invoice/go" -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
